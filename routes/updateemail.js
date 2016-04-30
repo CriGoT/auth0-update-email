c5d4e17cc738f44fe7af596a8f276215c0c3f07d
Cristofer Gonzales <cristofg@microsoft.com>
2016-04-30 19:21:05 -0300

    Adds first version of API to change email

--- routes/updateemail.js
+++ routes/updateemail.js
@@ -2,7 +2,8 @@ import express            from 'express'
 import {ManagementClient} from'auth0'
 import template           from '../views/update.jade'
 import jwt                from 'express-jwt'
-import url                from 'url';
+import util               from './utils'
+import bodyParser         from 'body-parser'
 
 const config = {
   AUTH0_DOMAIN: 'crigot.auth0.com',
@@ -11,7 +12,7 @@ const config = {
   AUTH0_CLIENT_SECRET: '',
   AUTH0_CONNECTION: 'Username-Password-Authentication',
 };
-const router = express.Router();
+
 const auth0client =new ManagementClient({
   token: config.AUTH0_MANAGEMENT_TOKEN,
   domain: config.AUTH0_DOMAIN
@@ -22,25 +23,24 @@ const authenticate = jwt({
   audience: config.AUTH0_CLIENT_ID
 });
 
-router.use('/api', authenticate);
+const router = express.Router();
+
+router.use('/api',
+    authenticate,
+    bodyParser.json({strict:true}));
+
 
 router.get('/',(req,res) =>
 {
-  console.log(url.parse(req.originalUrl));
-  const pathname = url.parse(req.originalUrl).pathname.replace(req.path, '');
-  const baseUrl = url.format({
-      protocol: 'https',
-      host:     req.get('host'),
-      pathname: pathname
-    });
   res.header("Content-Type", 'text/html');
-  res.status(200).send(template({baseUrl:baseUrl, config:config}));
+  res.status(200).send(template({baseUrl:util.getBaseUrl(req), config:config}));
 });
 
-router.get('/api/checkmail',(req, res)=>{
+router.get('/api/check',(req, res)=>{
   if (!req.query.m) {
     return res.status(400).json({"name":"BadRequestError","code":"mail_required","description":"You must provide a mail to validate","statusCode":400});
   }
+  console.log(req);
 
   auth0client
     .users
@@ -58,7 +58,36 @@ router.get('/api/checkmail',(req, res)=>{
         );
     })
     .catch((err)=>{
+      console.log(err);
       res.status(500).json({"name":"InternalError","code":"management_api_error","description":"The Managemente API returned an error","statusCode":500});
+    });
+
+});
+
+router.patch('/api/user',(req,res)=>{
+  if (!req.body || !req.body.email)
+  {
+    return res.status(400).json({"name":"BadRequestError","code":"mail_required","description":"You must provide a mail to update the user info","statusCode":400});
+  }
+
+  auth0client
+    .users
+    .update({id:req.user.sub},{email:req.body.email})
+    .then(data => {
+      auth0client
+        .jobs
+        .verifyEmail({user_id:req.user.sub})
+        .then(data => {
+          res.status(201).json({"status":"completed","message":"Your E-mail address has been updated you will shortly receive a message to verify it."});
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(201).json({"status":"pending","message":"Your E-mail address has been updated but the message to verify your new address was not sent."});
+        });
+    })
+    .catch((err)=>{
+      console.log(err);
+      res.status(500).json({"name":"InternalError","code":"management_api_error","description":"The Managemente API returned an error while updating the user","statusCode":500});
     });
 
 });
