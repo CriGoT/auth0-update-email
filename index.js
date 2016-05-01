9938f470e6f0e82689ddf66bbad8f2b21b6c13b3
Cristofer Gonzales <cristofg@microsoft.com>
2016-05-01 15:01:11 -0300

    Removes the Auth0 Management Panel extension code

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 import express  from 'express'
-import admin    from './routes/admin'
 import update   from './routes/update'
 import unless   from 'express-unless'
 
 export default function(config) {
   const app = express();
   const updateMiddleware = update(config);
-  const adminMiddleware = admin(config);
 
-  app.use('/update',updateMiddleware);
-  adminMiddleware.unless = unless;
-  app.use('/',adminMiddleware.unless({path:'/update*'}));
+  app.use('/',updateMiddleware);
 
   return app;
 }
